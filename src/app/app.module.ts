d4c0a5eaf00aaace7bbe427952c0c853a9b166bf
Fritchou2 <mohamedaziz.ferchichi@esprit.tn>
2022-09-25 12:41:59 +0100

    workshop 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './composant/header/header.component';
+import { FooterComponent } from './composant/footer/footer.component';
+
+import { ContentComponent } from './composant/content/content.component';
+import { ListUserComponent } from './composant/list-user/list-user.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    ListUserComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
